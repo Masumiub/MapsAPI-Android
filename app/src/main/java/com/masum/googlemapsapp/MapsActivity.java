c0dbbf8b72775037eba81c8b2c3cf9d7db1dde2d
Md. Masum Musfique <musfiquemasum8@gmail.com>
2022-11-12 18:40:09 +0600

    init

---
+++ app/src/main/java/com/masum/googlemapsapp/MapsActivity.java
@@ -0,0 +1,70 @@
+package com.masum.googlemapsapp;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.masum.googlemapsapp.databinding.ActivityMapsBinding;
+
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    List<Address> listGeoCoder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        try{
+            listGeoCoder = new Geocoder(this).getFromLocationName("Independent University Bangladesh Auditorium, Bashundhara Road, Dhaka", 1);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        double longitude = listGeoCoder.get(0).getLongitude();
+        double latitude = listGeoCoder.get(0).getLatitude();
+
+        Log.i("GOOGLE_MAP_TAG", "Address has longitude :::" + String.valueOf(longitude) + "Latitude" + String.valueOf(latitude));
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
